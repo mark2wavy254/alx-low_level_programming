00bcf7984054f7d62b7b72f08c547c40937ecd30
mark2wavy254 <muturimark16@gmail.com>
2024-01-12 17:38:46 +0300

    task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers passed to it
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		int num1 = atoi(argv[1]);
+		int num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
