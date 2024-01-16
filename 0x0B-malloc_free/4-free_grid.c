1ac6873f84a43259393b184ecee38b691c6b4d18
mark2wavy254 <muturimark16@gmail.com>
2024-01-16 16:41:46 +0300

    task4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * free_grid - frees a two dimensional grid
+ * @grid: input
+ * @height: input
+ *
+ * Return: Nothing.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == 0)
+	{
+		return;
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
