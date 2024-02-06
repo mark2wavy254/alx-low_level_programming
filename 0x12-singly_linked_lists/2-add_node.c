07982e7f9b69171b50538bd52769c1453fddb876
mark2wavy254 <muturimark16@gmail.com>
2024-02-06 18:53:49 +0300

    task2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -22,6 +22,7 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new_node->str = strdup(str);
+	new_node->len = strlen(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
@@ -30,5 +31,6 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node->next = *head;
 	*head = new_node;
+
 	return (new_node);
 }
