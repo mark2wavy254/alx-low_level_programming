a25e906a211e82383068ec1ce34eebc5e1b93a31
mark2wavy254 <muturimark16@gmail.com>
2024-02-07 10:48:56 +0300

    task8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -10,4 +10,15 @@
  * 0 if the list is empty.
  */
 
+int sum_listint(listint_t *head)
+{
+	const listint_t *current = head;
+	unsigned int sum = 0;
 
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
